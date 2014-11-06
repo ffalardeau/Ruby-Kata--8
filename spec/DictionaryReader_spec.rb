60e4ef6ff2328795a1d93c2d392164a87b4196f6
Francis Falardeau <francis.falardeau@gmail.com>
2014-11-06 09:32:46 -0500

    using smaller word list file for unit tests, more test restructuring, remove return keyword in tool classes

--- spec/DictionaryReader_spec.rb
+++ spec/DictionaryReader_spec.rb
@@ -1,36 +1,38 @@
 require 'spec_helper'
 
-describe DictionaryReader, "#get_all_words" do
-  context 'when the dictionary is empty' do
-    it 'should return 0 words' do
-      subject.read(File.dirname(__FILE__)+"/../emptywordlist.txt")
-      expect(subject.get_all_words.count).to eq(0)
-    end
-  end
-
-  context 'when the dictionary is not found' do
-    it 'should return 0 words' do
-      subject.read("")
-      expect(subject.get_all_words.count).to eq(0)
-    end
-  end
-
-  context 'when the dictionay is found and not empty' do
-    before(:each) do
-      subject.read(File.dirname(__FILE__)+"/../wordlist.txt")
+describe DictionaryReader do
+  context 'when trying to get all words' do
+    context 'and the dictionary is empty' do
+      it 'should return 0 words' do
+        subject.read(File.dirname(__FILE__)+"/support/empty_word_list.txt")
+        expect(subject.get_all_words.count).to eq(0)
+      end
     end
 
-    it "should have the same number of words as it's source" do
-      expect(subject.get_all_words.count).to eq(45433)
+    context 'and the dictionary is not found' do
+      it 'should return 0 words' do
+        subject.read("")
+        expect(subject.get_all_words.count).to eq(0)
+      end
     end
 
-    it "should have the same first word as it's source" do
-      expect(subject.get_all_words[0]).to eq("Aarhus")
-    end
+    context 'and the dictionay is found and not empty' do
+      before(:each) do
+        subject.read(File.dirname(__FILE__)+"/support/2_word_list.txt")
+      end
 
-    it "should have the same last word as it's source" do
-      words = subject.get_all_words
-      expect(words[words.count - 1]).to eq("Zurich")
+      it "should have the same number of words as it's source" do
+        expect(subject.get_all_words.count).to eq(2)
+      end
+
+      it "should have the same first word as it's source" do
+        expect(subject.get_all_words[0]).to eq("hello")
+      end
+
+      it "should have the same last word as it's source" do
+        words = subject.get_all_words
+        expect(words[words.count - 1]).to eq("you")
+      end
     end
   end
 
--- spec/WordArrayFilter_spec.rb
+++ spec/WordArrayFilter_spec.rb
@@ -1,34 +1,67 @@
 require 'spec_helper'
 
 describe WordArrayFilter do
-  let(:word_list) {["hello", "crazy", "Adonis", "Bye", "advise"]}
 
-  context 'when we have a list containing 2 six character words' do
-    it 'should return 2 words when specifying 6 as the number of characters' do
-      expect(subject.get_words_with_x_letter(6, word_list).count).to eq(2)
+  context 'when filtering words with an exact number of letters' do
+    context 'and we have an empty list' do
+      let(:word_list) {[]}
+
+      it 'should always return 0 words' do
+        expect(subject.get_words_with_x_letter(2, word_list).count).to eq(0)
+        expect(subject.get_words_with_x_letter(10, word_list).count).to eq(0)
+        expect(subject.get_words_with_x_letter(0, word_list).count).to eq(0)
+      end
     end
 
-    it 'should return 0 words when specifying 8 as the number of characters' do
-      expect(subject.get_words_with_x_letter(8, word_list).count).to eq(0)
+    context 'and we have a list containing 2 six letter words' do
+      let(:word_list) {["hello", "crazy", "Adonis", "Bye", "advise"]}
+
+      it 'should return 2 words when specifying 6 as the number of letters' do
+        expect(subject.get_words_with_x_letter(6, word_list).count).to eq(2)
+      end
+
+      it 'should return 0 words when specifying 8 as the number of letters' do
+        expect(subject.get_words_with_x_letter(8, word_list).count).to eq(0)
+      end
+
+      it 'should return 0 words when specifying 0 as the number of letters' do
+        expect(subject.get_words_with_x_letter(0, word_list).count).to eq(0)
+      end
+    end
+
+    context 'and the list is not an array' do
+      let(:word_list) {{}}
+
+      it 'should always return 0 words' do
+        expect(subject.get_words_with_x_letter(0, word_list).count).to eq(0)
+      end
     end
   end
 
-  context 'when filtering words with less than X number of characters' do
-    context 'when looking for words with less than 5 characters' do
+  context 'when filtering words with less than X number of letters' do
+    let(:word_list) {["hello", "crazy", "Adonis", "Bye", "advise"]}
+
+    context 'and looking for words with less than 5 letters' do
       it 'should return 1 words' do
         expect(subject.get_words_with_less_than_x_letter(5, word_list).count).to eq(1)
       end
     end
 
-    context 'when looking for words with less than 2 characters' do
+    context 'and looking for words with less than 2 letters' do
       it 'should return 0 words' do
         expect(subject.get_words_with_less_than_x_letter(2, word_list).count).to eq(0)
       end
     end
 
-    context 'when looking for words with less the 6 characters' do
+    context 'and looking for words with less the 6 letters' do
       it 'should return 3 words' do
         expect(subject.get_words_with_less_than_x_letter(6, word_list).count).to eq(3)
+      end
+    end
+
+    context 'and looking for words with less the 0 letters' do
+      it 'should return 0 words' do
+        expect(subject.get_words_with_less_than_x_letter(0, word_list).count).to eq(0)
       end
     end
   end
@@ -55,6 +88,10 @@ describe WordArrayFilter do
 
       it "should return 1 word that starts with 'b'" do
         expect(subject.get_words_that_starts_with_letter('b', word_list).count).to eq(1)
+      end
+
+      it "should return 0 words that starts with '.'" do
+        expect(subject.get_words_that_starts_with_letter('.', word_list).count).to eq(0)
       end
     end
   end
